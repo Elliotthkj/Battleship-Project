4c25b98358829c3d109885c32a2b1685eafaf823
Elliott Johnson <elliotthkj1991@gmail.com>
2023-03-25 14:43:28 -0700

    created pseudo code via JS comments and added basic starter variables

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// store main game data/game state
+let gameData = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+  ],
+};
+
+// display results of guess to the player (msg and miss/hit)
+
+//track amount of guesses and win condition
+
+// function to parse a guess from the user
+
 // event handlers
 // handle fire button
 // handle key press
 
-// initialize the game/assigning event handlers and generate ship locations
+// initialize the game/assign event handlers and generate ship locations
