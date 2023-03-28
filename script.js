6e519bc18ebbfff8e03ed5bd69dab88093f5babc
Elliott Johnson <elliotthkj1991@gmail.com>
2023-03-27 19:29:59 -0700

    added object to access message display area and update cells with user guesses

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let gameData = {
   shipLength: 3,
   shipsSunk: 0,
 
-  ships: [ // array thats storing the ship locations and if the player has hit those ship locations
+  ships: [
+    // array thats storing the ship locations and if the player has hit those ship locations
     { locations: [0, 0, 0], hits: ["", "", ""] },
     { locations: [0, 0, 0], hits: ["", "", ""] },
     { locations: [0, 0, 0], hits: ["", "", ""] },
@@ -13,6 +14,25 @@ let gameData = {
 };
 
 // display results of guess to the player (msg and miss/hit)
+let resultsDisplay = {
+  // will display msg based on the msg passed to the function based on the users guess results
+  displayMessage: function (msg) {
+    let messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+
+  // will take users guess and update the cell that was guessed with 'hit' img
+  displayHit: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+
+  // will take users guess and update the cell that was guessed with 'miss' img
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
 
 //track amount of guesses and win condition
 
